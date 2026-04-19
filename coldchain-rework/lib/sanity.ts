66bcab80cf2732021a9991ebff31aeba34f05a63
JDOM10 <juandiegoosorio10@hotmail.com>
2026-04-19 14:27:09 -0500

    refactor: remove @sanity/client dependency and related code

--- coldchain-rework/lib/sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client'
-
-export const client = createClient({
-  projectId: '44abjvhd',
-  dataset: 'production',
-  apiVersion: '2024-01-01',
-  useCdn: true,
-})
